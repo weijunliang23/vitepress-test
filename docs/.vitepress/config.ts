ca106b29c0e254d513347c26d25f390c9787f6f0
code_wei <code_wei>
2023-03-13 21:35:28 +0800

    reactor: 改变工作目录

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   base: ".",
   title: "My Awesome Project",
   description: "A VitePress Site",
+  srcDir: "src",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
