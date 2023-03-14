e8a37dc7202ff1313a81e1525abb59aef2f4d862
code_wei <code_wei>
2023-03-14 21:00:38 +0800

    feat: config.ts base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitepress"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  // base: ".",
+  base: "/",
   title: "My Awesome Project",
   cleanUrls: true,
   description: "A VitePress Site",
